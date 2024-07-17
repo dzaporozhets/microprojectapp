4f5d9d4933ae0eb0cf42098aae4cd8fd3e07f216
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-07-17 12:50:19 +0000

    Add large dev seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,42 @@ if ENV['DEV_DATA'].present?
   ]
 
   projects.each do |project_name|
-    Project.create!(name: project_name, user: user)
+    project = Project.create!(name: project_name, user: user)
+    project.create_sample_tasks
   end
+end
+
+if ENV['DEV_DATA_BIG'].present? && Rails.env.development?
+  require 'factory_bot_rails'
+
+  # All objects will be created for one user
+  user = User.first
+
+  puts "Creating a lot of data for #{user.email}"
+
+  project = Project.find_by(name: 'Large Project')
+
+  unless project && project.tasks.count > 500
+    project ||= FactoryBot.create(:project, name: 'Large Project', user: user)
+
+    5.times { project.users << FactoryBot.create(:user) }
+
+    99.times do |i|
+      FactoryBot.create(:task, name: "task-open-#{i}", project: project, user: project.users.sample)
+    end
+
+    900.times do |i|
+      FactoryBot.create(:task, name: "task-done-#{i}", project: project, user: project.users.sample, done: true)
+    end
+
+    puts '* 1 large project created'
+  end
+
+  50.times do |i|
+    project = FactoryBot.create(:project, name: "Small Project #{i}", user: user)
+    project.create_sample_tasks
+    project.create_sample_links
+  end
+
+  puts '* 50 small projects created'
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     description { "Test Task Description" }
     association :user
     association :project
+
+    trait :with_random_name do
+      name { Faker::Name.unique.name }
+    end
   end
 end
