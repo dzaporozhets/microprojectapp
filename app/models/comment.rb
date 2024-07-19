a00f62792d6bc3bbe2d1f6fdadb67116a7397011
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-07-19 09:23:18 +0000

    Add comment model spec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,15 @@ class Comment < ApplicationRecord
   belongs_to :task
   belongs_to :user
 
+  validates :body, presence: true
+
   mount_uploader :attachment, AttachmentUploader
 
   def image_attachment?
     attachment.content_type.start_with?('image/')
+  end
+
+  def user_email
+    user.email
   end
 end
