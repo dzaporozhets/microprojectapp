74847d18c53605d8ff27297e8cee67fceecea0f8
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-07-22 11:55:41 +0000

    Further refactor layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,13 @@ module ApplicationHelper
     date.strftime("%A, #{day_with_ordinal}")
   end
 
-  def gravatar_tag(email, css_class = '', options = { size: 40, alt: 'Gravatar' })
+  def gravatar_tag(email, css_class = '', options = { size: 40, width: nil, alt: 'Gravatar' })
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
     size = options[:size]
+    width = options[:width] || size
     alt = options[:alt]
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: alt, class: "gravatar #{css_class}")
+    image_tag(gravatar_url, alt: alt, class: "gravatar #{css_class}", width: width)
   end
 
   def flash_css(type)
