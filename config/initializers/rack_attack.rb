da34172e9c3b4defc327aadbc7152d58a2c8e279
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-07-26 14:38:30 +0000

    Update rack attack settings
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,21 +1,26 @@
 class Rack::Attack
   # Throttle POST requests to /users/password by IP address
-  throttle('req/ip', limit: 5, period: 60.seconds) do |req|
+  throttle('req/ip/password', limit: 10, period: 60.seconds) do |req|
     req.ip if req.path == '/users/password' && req.post?
   end
 
+  # Throttle POST requests to /users/sign_in by IP address
+  throttle('req/ip/sign_in', limit: 10, period: 60.seconds) do |req|
+    req.ip if req.path == '/users/sign_in' && req.post?
+  end
+
   # Throttle POST requests to /users/password by email
-  throttle('password_reset/email', limit: 5, period: 3600.seconds) do |req|
+  throttle('password_reset/email', limit: 10, period: 3600.seconds) do |req|
     req.params['user']['email'].presence if req.path == '/users/password' && req.post?
   end
 
   # Throttle POST requests to /users/confirmation by IP address
-  throttle('req/ip/confirmation', limit: 5, period: 60.seconds) do |req|
+  throttle('req/ip/confirmation', limit: 10, period: 60.seconds) do |req|
     req.ip if req.path == '/users/confirmation' && req.post?
   end
 
   # Throttle POST requests to /users/confirmation by email
-  throttle('confirmation/email', limit: 5, period: 3600.seconds) do |req|
+  throttle('confirmation/email', limit: 10, period: 3600.seconds) do |req|
     req.params['user']['email'].presence if req.path == '/users/confirmation' && req.post?
   end
 
