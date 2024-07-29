3ac55c2c7a5c3d86a04d693e2e927042e88638f2
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-07-29 12:49:57 +0000

    Fix schedule week /month overlap
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/project/schedule_controller.rb
+++ app/controllers/project/schedule_controller.rb
@@ -10,11 +10,18 @@ class Project::ScheduleController < Project::BaseController
     # Get tasks that are due this week (without tasks that are today)
     @tasks_due_this_week = tasks.where(due_date: Date.current.tomorrow..Date.current.end_of_week)
 
-    # Get tasks that are due this month (without tasks that are today and this week)
-    @tasks_due_this_month = tasks.where(due_date: Date.current.end_of_week.next_day..Date.current.end_of_month)
+    # If month ends sooner than week, we skip whole "This month" section
+    if Date.current.end_of_week > Date.current.end_of_month
+      @tasks_due_this_month = Task.none
+    else
+      # Get tasks that are due this month (without tasks that are today and this week)
+      @tasks_due_this_month = tasks.where(due_date: Date.current.end_of_week.next_day..Date.current.end_of_month)
+    end
 
     # Get tasks from today will end of the year
     @tasks_due_this_year = tasks.where(due_date: Date.current.end_of_month.next_day..Date.current.end_of_year)
+      .where.not(id: @tasks_due_this_week)
+      .where.not(id: @tasks_due_this_month)
   end
 
   def tasks
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -102,4 +102,6 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+
+  config.include ActiveSupport::Testing::TimeHelpers
 end
