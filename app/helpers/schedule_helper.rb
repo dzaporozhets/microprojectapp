d24a8693becbfca2e2c947c5ca3111a32d35f65c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-07-30 03:16:52 +0000

    Update due date selectbox with different options

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -4,10 +4,9 @@ module ScheduleHelper
       ['No Due Date', nil],
       ["Tomorrow (+1 day)", 1.day.from_now.to_date],
       ["Monday (+#{days_until_next_monday} days)", next_monday.to_date],
-      ["End of the Week (+#{days_until_end_of_week} days)", end_of_week.to_date],
-      ["End of the Month (+#{days_until_end_of_month} days)", end_of_month.to_date],
-      ["End of the Year (+#{days_until_end_of_year} days)", end_of_year.to_date],
-      ["Two Weeks from Now (+14 days)", 2.weeks.from_now.to_date]
+      ["Two Weeks from Now (+14 days)", 2.weeks.from_now.to_date],
+      ["Four Weeks from Now (+28 days)", 4.weeks.from_now.to_date],
+      ["Eight Weeks from Now (+56 days)", 8.weeks.from_now.to_date]
     ]
 
     if existing_due_date
@@ -30,29 +29,5 @@ module ScheduleHelper
 
   def next_monday
     Date.today + days_until_next_monday
-  end
-
-  def days_until_end_of_week
-    (Date.today.end_of_week - Date.today).to_i
-  end
-
-  def end_of_week
-    Date.today.end_of_week
-  end
-
-  def days_until_end_of_month
-    (Date.today.end_of_month - Date.today).to_i
-  end
-
-  def end_of_month
-    Date.today.end_of_month
-  end
-
-  def days_until_end_of_year
-    (Date.today.end_of_year - Date.today).to_i
-  end
-
-  def end_of_year
-    Date.today.end_of_year
   end
 end
--- spec/helpers/schedule_helper_spec.rb
+++ spec/helpers/schedule_helper_spec.rb
@@ -20,31 +20,15 @@ RSpec.describe ScheduleHelper, type: :helper do
       expect(options).to include(["Monday (+#{days_until_monday} days)", next_monday])
     end
 
-    it 'includes "End of the Week" option' do
-      end_of_week = Date.today.end_of_week
-      days_until_end_of_week = (end_of_week - Date.today).to_i
-      options = helper.due_date_options
-      expect(options).to include(["End of the Week (+#{days_until_end_of_week} days)", end_of_week])
-    end
-
-    it 'includes "End of the Month" option' do
-      end_of_month = Date.today.end_of_month
-      days_until_end_of_month = (end_of_month - Date.today).to_i
-      options = helper.due_date_options
-      expect(options).to include(["End of the Month (+#{days_until_end_of_month} days)", end_of_month])
-    end
-
-    it 'includes "End of the Year" option' do
-      end_of_year = Date.today.end_of_year
-      days_until_end_of_year = (end_of_year - Date.today).to_i
-      options = helper.due_date_options
-      expect(options).to include(["End of the Year (+#{days_until_end_of_year} days)", end_of_year])
-    end
-
     it 'includes "Two Weeks from Now" option' do
       two_weeks_from_now = 2.weeks.from_now.to_date
       options = helper.due_date_options
       expect(options).to include(["Two Weeks from Now (+14 days)", two_weeks_from_now])
+    end
+
+    it 'includes "Four Weeks from Now" option' do
+      options = helper.due_date_options
+      expect(options).to include(["Four Weeks from Now (+28 days)", 4.weeks.from_now.to_date])
     end
 
     it 'prepends existing due date if provided' do
@@ -53,24 +37,6 @@ RSpec.describe ScheduleHelper, type: :helper do
       days_difference = (existing_due_date.to_date - Date.today).to_i
       options = helper.due_date_options(existing_due_date)
       expect(options.first).to eq(["#{human_readable_date} (+#{days_difference} days)", existing_due_date])
-    end
-
-    context 'when current week includes the next month' do
-      it 'includes correct options' do
-        # Set today is a Monday
-        today = Date.new(2024, 7, 29)
-        allow(Date).to receive(:today).and_return(today)
-
-        # Next month is 3 days away
-        wednesday = today + 2.days
-        end_of_week = today.end_of_week
-
-        options = helper.due_date_options
-
-        expect(options).to include(["Monday (+7 days)", today + 7.days])
-        expect(options).to include(["End of the Week (+6 days)", end_of_week])
-        expect(options).to include(["End of the Month (+2 days)", wednesday])
-      end
     end
   end
 end
