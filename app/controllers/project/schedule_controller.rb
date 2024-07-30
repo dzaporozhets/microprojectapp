815867b689e6ec2b892782dfdf7cf9619881eb6f
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-07-30 02:57:56 +0000

    Remake schedule page

--- app/controllers/project/schedule_controller.rb
+++ app/controllers/project/schedule_controller.rb
@@ -3,25 +3,15 @@ class Project::ScheduleController < Project::BaseController
 
   def show
     # TODO: Move query logic in the model and add tests
+    #
+    # Get tasks that are due this month (including past tasks up to today).
+    @tasks_due_this_month = tasks.where(due_date: Date.current.beginning_of_month..Date.current.end_of_month)
 
-    # Get tasks that are due today (including past tasks here).
-    @tasks_due_today = tasks.where('due_date <= ?', Date.current)
+    # Get tasks that are due next month.
+    @tasks_due_next_month = tasks.where(due_date: Date.current.next_month.beginning_of_month..Date.current.next_month.end_of_month)
 
-    # Get tasks that are due this week (without tasks that are today)
-    @tasks_due_this_week = tasks.where(due_date: Date.current.tomorrow..Date.current.end_of_week)
-
-    # If month ends sooner than week, we skip whole "This month" section
-    if Date.current.end_of_week > Date.current.end_of_month
-      @tasks_due_this_month = Task.none
-    else
-      # Get tasks that are due this month (without tasks that are today and this week)
-      @tasks_due_this_month = tasks.where(due_date: Date.current.end_of_week.next_day..Date.current.end_of_month)
-    end
-
-    # Get tasks from today will end of the year
-    @tasks_due_this_year = tasks.where(due_date: Date.current.end_of_month.next_day..Date.current.end_of_year)
-      .where.not(id: @tasks_due_this_week)
-      .where.not(id: @tasks_due_this_month)
+    # Get tasks that are due the month after next.
+    @tasks_due_month_after_next = tasks.where(due_date: Date.current.next_month.next_month.beginning_of_month..Date.current.next_month.next_month.end_of_month)
   end
 
   def tasks
--- spec/features/project_schedule_spec.rb
+++ spec/features/project_schedule_spec.rb
@@ -13,55 +13,30 @@ RSpec.feature "Project Schedule", type: :feature do
       past_task = create(:task, name: "Past Task", due_date: Date.current - 1.day, project: project, user: user)
       today_task = create(:task, name: "Today Task", due_date: Date.current, project: project, user: user)
       tomorrow_task = create(:task, name: "Tomorrow Task", due_date: Date.current + 1.day, project: project, user: user)
-      end_of_week_task = create(:task, name: "End of Week Task", due_date: Date.current.end_of_week, project: project, user: user)
-      next_week_task = create(:task, name: "Next Week Task", due_date: Date.current.next_week.beginning_of_week, project: project, user: user)
       end_of_month_task = create(:task, name: "End of Month Task", due_date: Date.current.end_of_month, project: project, user: user)
       next_month_task = create(:task, name: "Next Month Task", due_date: Date.current.next_month.beginning_of_month, project: project, user: user)
-      end_of_year_task = create(:task, name: "End of Year Task", due_date: Date.current.end_of_year, project: project, user: user)
+      month_after_next_task = create(:task, name: "Month After Next Task", due_date: Date.current.next_month.next_month.beginning_of_month, project: project, user: user)
 
       visit project_schedule_path(project)
 
-      within("section", text: "Today") do
+      within("section", text: Date.current.strftime("%B")) do
         expect(page).to have_content("Past Task")
         expect(page).to have_content("Today Task")
-        expect(page).not_to have_content("Tomorrow Task")
-      end
-
-      within("section", text: "This Week") do
         expect(page).to have_content("Tomorrow Task")
-        expect(page).to have_content("End of Week Task")
-        expect(page).not_to have_content("Next Week Task")
-      end
-
-      within("section", text: "This Month") do
-        expect(page).to have_content("Next Week Task")
         expect(page).to have_content("End of Month Task")
         expect(page).not_to have_content("Next Month Task")
       end
 
-      within("section", text: "This Year") do
+      within("section", text: Date.current.next_month.strftime("%B")) do
         expect(page).to have_content("Next Month Task")
-        expect(page).to have_content("End of Year Task")
-      end
-    end
-  end
-
-  scenario "User visits the schedule page when month ends before week" do
-    travel_to Date.new(2024, 7, 30) do
-      end_of_week_task = create(:task, name: "End of Week Task", due_date: Date.current.end_of_week, project: project, user: user)
-
-      visit project_schedule_path(project)
-
-      within("section", text: "This Week") do
-        expect(page).to have_content("End of Week Task")
+        expect(page).not_to have_content("End of Month Task")
+        expect(page).not_to have_content("Month After Next Task")
       end
 
-      expect(page).not_to have_content("This Month")
-
-      within("section", text: "This Year") do
-        expect(page).not_to have_content("End of Week Task")
+      within("section", text: Date.current.next_month.next_month.strftime("%B")) do
+        expect(page).to have_content("Month After Next Task")
+        expect(page).not_to have_content("Next Month Task")
       end
     end
   end
 end
-
