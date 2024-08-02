2e4ebd9966d78fd1f7a600d0dd9a51fb9c667a8c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-02 11:56:31 +0000

    Add ability to export project tasks
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/project/export_controller.rb
+++ app/controllers/project/export_controller.rb
@@ -3,5 +3,17 @@ class Project::ExportController < Project::BaseController
   end
 
   def create
+    tasks_data = project.tasks.as_json(except: [:id, :user_id, :project_id])
+
+    export_data = {
+      project_name: project.name,
+      tasks: tasks_data
+    }
+
+    filename = "microprojectapp-project-#{project.id}-timestamp-#{Time.now.to_i}.json"
+
+    respond_to do |format|
+      format.json { send_data export_data.to_json, filename: filename, type: 'application/json', disposition: 'attachment' }
+    end
   end
 end
