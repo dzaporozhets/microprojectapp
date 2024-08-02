10c95957c1dec7a45b62901daf04a882731c326b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-02 09:44:35 +0000

    Extract link title from the url
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,21 @@ class Link < ApplicationRecord
   validates :url, presence: true, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }
 
   def safe_title
-    @safe_title ||= title.present? ? title : url
+    @safe_title ||= title.present? ? title : title_from_url
+  end
+
+  private
+
+  def title_from_url
+    uri = URI.parse(url)
+    segments = uri.path.split('/')
+    title_segment = segments.last
+    title_text = title_segment ? title_segment.split('_').map(&:capitalize).join(' ') : ''
+
+    if title_text.present?
+      title_text
+    else
+      url
+    end
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:project) }
+
+  it { should validate_presence_of(:url) }
+
+  describe '#safe_title' do
+    context 'when title is present' do
+      it 'returns the title' do
+        link = build(:link, title: 'Custom Title')
+        expect(link.safe_title).to eq('Custom Title')
+      end
+    end
+
+    context 'when title is not present' do
+      it 'generates a title from the URL' do
+        link = build(:link, title: nil, url: 'https://en.wikipedia.org/wiki/Time_management')
+        expect(link.safe_title).to eq('Time Management')
+      end
+
+      it 'handles URLs with no path segments gracefully' do
+        link = build(:link, title: nil, url: 'https://example.com/')
+        expect(link.safe_title).to eq('https://example.com/')
+      end
+
+      it 'handles URLs with complex paths' do
+        link = build(:link, title: nil, url: 'https://example.com/some/complex_path-here')
+        expect(link.safe_title).to eq('Complex Path-here')
+      end
+
+      it 'handles URLs with file extension' do
+        link = build(:link, title: nil, url: 'https://example.com/some/complex_path-here.pdf')
+        expect(link.safe_title).to eq('Complex Path-here.pdf')
+      end
+
+      it 'handles URLs with underscores in path' do
+        link = build(:link, title: nil, url: 'https://example.com/some_complex_path_here')
+        expect(link.safe_title).to eq('Some Complex Path Here')
+      end
+    end
+  end
 end
