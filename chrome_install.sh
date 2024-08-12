1c8180e960fe4c9ab1a5026fa7dfb4d110c254fe
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-12 09:31:00 +0000

    Install dependencies and mode headless options
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- chrome_install.sh
+++ chrome_install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Install necessary dependencies
+apt-get update -qq
+apt-get install -y \
+  libnss3 \
+  libgconf-2-4 \
+  libxi6 \
+  libxcursor1 \
+  libxcomposite1 \
+  libasound2 \
+  libxdamage1 \
+  libxtst6 \
+  libglib2.0-0 \
+  libxrandr2 \
+  libxss1 \
+  libx11-xcb1
+
 # Add the Google Chrome signing key and repo
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
@@ -15,7 +31,7 @@ CHROMEDRIVER_VERSION="114.0.5735.90"
 echo "Using hardcoded Chromedriver version: $CHROMEDRIVER_VERSION"
 
 echo "Installing chromedriver from https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
-wget https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+wget -q https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 rm chromedriver_linux64.zip
