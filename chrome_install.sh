0fc1521921fd069673cbdd818b84a201b217618a
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-12 09:38:49 +0000

    Install chrome from wget
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- chrome_install.sh
+++ chrome_install.sh
@@ -14,28 +14,30 @@ apt-get install -y \
   libglib2.0-0 \
   libxrandr2 \
   libxss1 \
-  libx11-xcb1
-
-# Add the Google Chrome signing key and repo
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
-apt-get update -qq
+  libx11-xcb1 \
+  wget \
+  curl \
+  unzip
 
 # Hardcoded Chrome version
 CHROME_VERSION="114.0.5735.198"
 echo "Using hardcoded Chrome version: $CHROME_VERSION"
 
-# Install the specific version of Google Chrome
-apt-get install -y google-chrome-stable=$CHROME_VERSION-1
+# Download and install the specific version of Google Chrome
+wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
+dpkg -i google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
+apt-get install -f -y  # Install any dependencies that might be missing
+rm google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
 
 # Hardcoded Chromedriver version
 CHROMEDRIVER_VERSION="114.0.5735.90"
 echo "Using hardcoded Chromedriver version: $CHROMEDRIVER_VERSION"
 
 echo "Installing chromedriver from https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
-wget -q https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
 rm chromedriver_linux64.zip
 
 echo "Chrome and Chromedriver setup complete."
+
