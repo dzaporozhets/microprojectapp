4f54fcaa3970c166d8b5291affe05fa2ebb90de8
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-12 06:35:19 +0000

    Move schrome install to the script
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ chrome_install.sh
@@ -0,0 +1,14 @@
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+apt-get update -qq
+apt-get install -y google-chrome-stable
+
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
+echo "Chrome version installed: $CHROME_VERSION"
+
+CHROMEDRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
+if [ -z "$CHROMEDRIVER_VERSION" ]; then echo "No matching Chromedriver version found for Chrome $CHROME_VERSION"; exit 1; fi
+wget -q https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+rm chromedriver_linux64.zip
