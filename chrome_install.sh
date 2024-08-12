e64ec1bcc4feb9a6d824d23bbbd56a2adc43492d
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-12 09:36:13 +0000

    Install hardcoded version of chrome
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- chrome_install.sh
+++ chrome_install.sh
@@ -20,11 +20,13 @@ apt-get install -y \
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
 apt-get update -qq
-apt-get install -y google-chrome-stable
 
 # Hardcoded Chrome version
 CHROME_VERSION="114.0.5735.198"
 echo "Using hardcoded Chrome version: $CHROME_VERSION"
+
+# Install the specific version of Google Chrome
+apt-get install -y google-chrome-stable=$CHROME_VERSION-1
 
 # Hardcoded Chromedriver version
 CHROMEDRIVER_VERSION="114.0.5735.90"
