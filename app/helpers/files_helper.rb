f467cf66816438db20667980bfe20c1daf6762a3
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-19 06:57:28 +0000

    Move file select styles to css
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/helpers/files_helper.rb
+++ app/helpers/files_helper.rb
@@ -21,13 +21,10 @@ module FilesHelper
     f.file_field :project_files,
       multiple: true, required: true,
       accept: allowed_file_types,
-      class: "appearance-none w-full
-            text-gray-700 dark:text-gray-300 leading-tight focus:outline-none
-            focus:shadow-outline file:mr-5 file:py-2 file:px-4
+      class: "appearance-none file-select w-full
+            leading-tight file:mr-5 file:py-2 file:px-4
             file:rounded-md file:border-0
-            file:text-sm file:font-medium
-            file:bg-gray-100 dark:file:bg-gray-800
-            file:text-gray-700 dark:file:text-gray-300"
+            file:text-sm file:font-medium"
   end
 end
 
