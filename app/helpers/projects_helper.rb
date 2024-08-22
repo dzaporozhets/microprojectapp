055e6c2980a89af2c9af89063f5b81d35167944a
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-22 19:53:15 +0000

    Move activity tab under project sidebar links
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -5,10 +5,6 @@ module ProjectsHelper
       { name: 'Schedule', path: project_schedule_path(@project) }
     ]
 
-    unless @project.personal?
-      tabs << { name: 'Activity', path: project_activity_path(@project) }
-    end
-
     render_tabs(tabs, selected)
   end
 end
