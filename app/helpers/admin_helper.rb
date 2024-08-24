246909fcbfd7a51eb7fb171af8938708fde8ffd9
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2024-08-24 09:09:15 +0000

    ADd minor ui improvements
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,10 @@
 module AdminHelper
+  def admin_tabs(selected = nil)
+    tabs = [
+      { name: 'Application', path: admin_path },
+      { name: 'Users', path: admin_users_path }
+    ]
+
+    render_tabs(tabs, selected)
+  end
 end
