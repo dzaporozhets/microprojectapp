3179d4a51fa5d68fd221835373a2ee2c4e61b0b9
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2025-02-05 15:55:16 +0000

    Update task UI

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -13,8 +13,8 @@ module ThemeHelper
 
   def task_css(task)
     base_css = 'border shadow-sm dark:shadow-gray-800 rounded-md px-3 py-1.5 '.freeze
-    task_done_css = 'bg-gray-200 dark:bg-gray-950 border-gray-200 dark:border-gray-800 line-through dark:decoration-gray-100'.freeze
-    task_todo_css = 'bg-gray-100 dark:bg-gray-900 border-gray-100 dark:border-gray-800'.freeze
+    task_done_css = 'bg-gray-100 dark:bg-gray-950 border-gray-50 dark:border-gray-800 line-through dark:decoration-gray-400 text-gray-600 dark:text-gray-400'.freeze
+    task_todo_css = 'bg-white dark:bg-gray-900 border-gray-100 dark:border-gray-800 text-black dark:text-neutral-200'.freeze
 
     base_css + (task.done ? task_done_css : task_todo_css)
   end
