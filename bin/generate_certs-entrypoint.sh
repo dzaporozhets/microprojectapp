653218f5ac60bfc1ae1a2597dbf5b3aa07c9be99
Mykyta Dynnik <mykytadynnik@gmail.com>
2025-03-27 16:06:02 +0200

    modified generate_certs-entrypoint.sh

--- bin/generate_certs-entrypoint.sh
+++ bin/generate_certs-entrypoint.sh
@@ -11,7 +11,7 @@ if [ ! -f "$CERT_KEY" ] || [ ! -f "$CERT_CRT" ]; then
   mkdir -p "$CERTS_DIR"
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout "$CERT_KEY" -out "$CERT_CRT" \
-    -subj "/C=UA/ST=Kyiv/L=Kyiv/O=MyApp/OU=IT Department/CN=myapp.local"
+    -subj "/C=/ST=/L=/O=MyApp/OU=IT Department/CN=myapp.local"
   echo "SSL certificates generated successfully."
 else
   echo "SSL certificates already exist."
