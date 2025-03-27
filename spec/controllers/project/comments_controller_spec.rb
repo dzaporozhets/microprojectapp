a9be7763e62d686c9db71b6e9ce1e7e804c0fbe8
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2025-03-27 16:31:51 +0200

    Add comment destroy controller spec

---
+++ spec/controllers/project/comments_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe Project::CommentsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:project) { create(:project, user: user) }
+  let(:task) { create(:task, project: project, user: user) }
+  let!(:comment) { create(:comment, task: task, user: user) }
+  let(:other_user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  describe "DELETE #destroy" do
+    context "with HTML format" do
+      it "destroys the comment" do
+        expect {
+          delete :destroy, params: {
+            project_id: project.id,
+            task_id: task.id,
+            id: comment.id
+          }
+        }.to change(Comment, :count).by(-1)
+
+        expect(response).to redirect_to(details_project_task_path(project, task))
+        expect(flash[:notice]).to eq('Comment was successfully deleted.')
+      end
+    end
+
+    context "with Turbo Stream format" do
+      it "destroys the comment and responds with turbo_stream" do
+        expect {
+          delete :destroy, params: {
+            project_id: project.id,
+            task_id: task.id,
+            id: comment.id
+          }, format: :turbo_stream
+        }.to change(Comment, :count).by(-1)
+
+        expect(response.media_type).to eq('text/vnd.turbo-stream.html')
+      end
+    end
+
+    context "when user is not authorized" do
+      before do
+        sign_out user
+        sign_in other_user
+      end
+
+      it "raises RecordNotFound" do
+        expect {
+          delete :destroy, params: {
+            project_id: project.id,
+            task_id: task.id,
+            id: comment.id
+          }
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+end
