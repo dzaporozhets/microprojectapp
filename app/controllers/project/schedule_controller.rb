8b78da1ab2e8ba81ccec23f876e7108a037bd05a
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2025-07-27 22:50:26 +0200

    Add ability to randomly assign tasks to next month

--- app/controllers/project/schedule_controller.rb
+++ app/controllers/project/schedule_controller.rb
@@ -20,8 +20,19 @@ class Project::ScheduleController < Project::BaseController
   def saturate
     tasks_no_due = project.tasks.todo.no_due_date.order_by_star_then_old.limit(5)
 
+    # Use the month parameter if provided, otherwise default to current month
+    target_month = if params[:month].present?
+                     begin
+                       Date.strptime(params[:month], "%Y-%m")
+                     rescue Date::Error
+                       Date.current
+                     end
+                   else
+                     Date.current
+                   end
+
     tasks_no_due.each do |task|
-      task.update(due_date: (Date.current..Date.current.end_of_month).to_a.sample)
+      task.update(due_date: (target_month.beginning_of_month..target_month.end_of_month).to_a.sample)
     end
 
     redirect_to project_schedule_path(@project)
