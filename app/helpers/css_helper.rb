8aba68076e58c95a48b8d6a934f071d877541f27
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2025-07-29 23:36:01 +0200

    Refactor css a bit

--- app/helpers/css_helper.rb
+++ app/helpers/css_helper.rb
@@ -19,10 +19,10 @@ module CssHelper
     if task.done
       "px-3 py-1 text-sm text-gray-700 dark:text-gray-400" \
       " line-through dark:decoration-gray-400 decoration-gray-600" \
-      "  hover:bg-neutral-50 dark:hover:bg-gray-900"
+      "  hover:bg-neutral-50 dark:hover:bg-gray-800"
     else
       "px-3 py-1 text-sm text-black dark:text-gray-100" \
-        " hover:bg-neutral-50 dark:hover:bg-gray-900"
+        " hover:bg-neutral-50 dark:hover:bg-gray-800"
     end
   end
 
