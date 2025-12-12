01e6296d31616d15c72f44626dbcf78ead17beb7
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2025-12-12 08:05:44 +0200

    Add search results for notes and links

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -20,20 +20,29 @@ RSpec.describe "Search", type: :request do
 
     it "returns tasks from projects the user can access" do
       create(:task, project: project, user: user, name: "Ship search feature")
+      create(:note, project: project, user: user, title: "Search notes for Q3")
+      create(:link, project: project, user: user, title: "Search docs", url: "https://example.com/search-docs")
 
       get search_path(query: "search")
 
       expect(response).to have_http_status(:ok)
       expect(response.body).to include("Ship search feature")
       expect(response.body).to include(project.name)
+      expect(response.body).to include("Search notes for Q3")
+      expect(response.body).to include("Search docs")
     end
 
-    it "does not return tasks from other projects" do
-      create(:task, name: "Hidden task", project: create(:project), user: create(:user))
+    it "does not return resources from other projects" do
+      other_project = create(:project)
+      create(:task, name: "Hidden task", project: other_project, user: create(:user))
+      create(:note, title: "Hidden note", project: other_project, user: create(:user))
+      create(:link, title: "Hidden link", project: other_project, user: create(:user))
 
       get search_path(query: "Hidden")
 
       expect(response.body).not_to include("Hidden task")
+      expect(response.body).not_to include("Hidden note")
+      expect(response.body).not_to include("Hidden link")
     end
   end
 end
