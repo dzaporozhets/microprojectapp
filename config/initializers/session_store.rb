3a6997b1cd353eeffd5c6322f034ac20db5bc56c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2026-01-30 20:26:34 +0200

    Fix session store key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store,
-                                        key: '_microproject_session',
-                                        expire_after: 1.week
+Rails.application.config.session_store :cookie_store, expire_after: 1.week
