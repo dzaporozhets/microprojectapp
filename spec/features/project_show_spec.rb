f5473d1f3bbe79f2701f626a41798b5fcfd758a0
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2026-02-19 12:57:59 +0200

    Show archived project status on show page

--- spec/features/project_show_spec.rb
+++ spec/features/project_show_spec.rb
@@ -14,6 +14,14 @@ RSpec.feature "Project", type: :feature do
     expect(page).to have_text(project.name)
   end
 
+  scenario "Archived project shows archived notice on show page" do
+    project.update!(archived: true)
+
+    visit project_path(project)
+
+    expect(page).to have_text("This project is archived")
+  end
+
   scenario "Unauthorized user cannot visit the project" do
     another_user = create(:user)
     another_project = create(:project, user: another_user)
