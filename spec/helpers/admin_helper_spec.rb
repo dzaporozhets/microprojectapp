3f71217b95c696a7d9e4786e0b2ece6d58d5482b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2026-02-24 20:03:28 +0200

    Cleanup some specs

--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -1,15 +1,51 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the AdminHelper. For example:
-#
-# describe AdminHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe AdminHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#admin_config_uploads_storage' do
+    context 'when BUCKETEER_BUCKET_NAME is set' do
+      it 'returns AWS S3 (via Bucketeer)' do
+        allow(Rails.application.config).to receive(:app_settings).and_return(
+          aws_s3_bucket: 'my-bucket'
+        )
+
+        ClimateControl.modify(BUCKETEER_BUCKET_NAME: 'my-bucketeer-bucket') do
+          expect(helper.admin_config_uploads_storage).to eq('AWS S3 (via Bucketeer)')
+        end
+      end
+    end
+
+    context 'when S3 is configured without Bucketeer' do
+      it 'returns AWS S3' do
+        allow(Rails.application.config).to receive(:app_settings).and_return(
+          aws_s3_bucket: 'my-bucket'
+        )
+
+        ClimateControl.modify(BUCKETEER_BUCKET_NAME: nil) do
+          expect(helper.admin_config_uploads_storage).to eq('AWS S3')
+        end
+      end
+    end
+
+    context 'when local file storage is enabled' do
+      it 'returns Local' do
+        allow(Rails.application.config).to receive(:app_settings).and_return(
+          aws_s3_bucket: nil,
+          enable_local_file_storage: true
+        )
+
+        expect(helper.admin_config_uploads_storage).to eq('Local')
+      end
+    end
+
+    context 'when neither S3 nor local storage is configured' do
+      it 'returns Disabled' do
+        allow(Rails.application.config).to receive(:app_settings).and_return(
+          aws_s3_bucket: nil,
+          enable_local_file_storage: nil
+        )
+
+        expect(helper.admin_config_uploads_storage).to eq('Disabled')
+      end
+    end
+  end
 end
--- spec/models/project_user_spec.rb
+++ spec/models/project_user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:project) }
+
+  it 'is valid with valid attributes' do
+    project_user = build(:project_user)
+
+    expect(project_user).to be_valid
+  end
 end
