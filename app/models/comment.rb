be0f93b928a419028812fdc51f513b4230e61757
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2026-03-19 20:14:35 +0200

    Swithc comment uploads to active storage

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,12 +3,21 @@ class Comment < ApplicationRecord
   belongs_to :user, optional: false
 
   validates :body, presence: true
+  validate :acceptable_attachment
 
-  mount_uploader :attachment, AttachmentUploader
+  has_one_attached :attachment
 
   delegate :email, to: :user, prefix: true
 
   def removed?
     removed_at.present?
+  end
+
+  private
+
+  def acceptable_attachment
+    return unless attachment.attached?
+
+    errors.add(:attachment, 'is too large (max 5MB)') if attachment.blob.byte_size > 5.megabytes
   end
 end
